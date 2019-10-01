10ce76f667ffcadf0b1f80a78f9b84029ac997ab
chowder23 <benjamin2333@gmail.com>
2019-10-01 15:06:03 +0200

    még pár fubnkció hozzáadása, köztük fájlba kiírás is

--- src/DealerShip.kt
+++ src/DealerShip.kt
@@ -1,21 +1,34 @@
 import Car
+import java.io.File
+import java.nio.file.Path
+
 class DealerShip(val nev:String, var telephely:String, val adoszam:Int) {
 
-    var carsList = mutableListOf<Car>()
+    var allCarList = mutableListOf<Car>()
 
     fun addNewCar(kocsi:Car)
     {
-        carsList.add(kocsi)
+        allCarList.add(kocsi)
     }
-    fun getCars()= carsList
+    fun getCars()= allCarList
 
     fun avarageHorsepower():Int
     {
         var atlag:Int = 0
-        for(kocsi in carsList)
+        for(kocsi in allCarList)
         {
             atlag+=kocsi.HorsePower
         }
         return atlag
     }
+
+    fun SaveOut()
+    {
+        val myFile = File("saves.txt")
+        for (kocsi in allCarList)
+        {
+            myFile.appendText(kocsi.toString())
+        }
+    }
+
 }
