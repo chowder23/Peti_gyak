d6fb794ec129abf0339bf5aa1817b69b63723d76
chowder23 <benjamin2333@gmail.com>
2019-10-01 14:52:58 +0200

    néhány alap funkció

--- src/DealerShip.kt
+++ src/DealerShip.kt
@@ -1,4 +1,21 @@
+import Car
 class DealerShip(val nev:String, var telephely:String, val adoszam:Int) {
 
-    
+    var carsList = mutableListOf<Car>()
+
+    fun addNewCar(kocsi:Car)
+    {
+        carsList.add(kocsi)
+    }
+    fun getCars()= carsList
+
+    fun avarageHorsepower():Int
+    {
+        var atlag:Int = 0
+        for(kocsi in carsList)
+        {
+            atlag+=kocsi.HorsePower
+        }
+        return atlag
+    }
 }
