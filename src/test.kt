a769db49d91ff012df09bc1f72ec071fd6a7fa30
chowder23 <benjamin2333@gmail.com>
2019-10-01 15:17:34 +0200

    WORKING SHIT

--- src/test.kt
+++ src/test.kt
@@ -1,17 +1,8 @@
 fun main()
 {
 
+    var kocsiSzalon = DealerShip("szalon","Budapest",123456)
+    kocsiSzalon.addNewCar(Car("123-asd",CarType.FERRARI,500,50000000))
+    kocsiSzalon.SaveOut()
 }
-<<<<<<< HEAD
-/*<<<<<<< HEAD
-=======
 
->>>>>>> 624c98c8af457a37bf6bc11c489f7bb813becee2*/
-=======
-
-/*<<<<<<< HEAD
-=======
-
->>>>>>> 624c98c8af457a37bf6bc11c489f7bb813becee2
-*/
->>>>>>> 12c34f1dbaad2c2356136a7dd3ca7688a4e68eb4
