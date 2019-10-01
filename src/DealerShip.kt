248143dc4a0a3bf5299356599d5e8c137f82f01c
chowder23 <benjamin2333@gmail.com>
2019-10-01 18:47:31 +0200

    fájlba kimentés és beolvasás tükletesen működik

--- src/DealerShip.kt
+++ src/DealerShip.kt
@@ -25,10 +25,33 @@ class DealerShip(val nev:String, var telephely:String, val adoszam:Int) {
     fun SaveOut()
     {
         val myFile = File("saves.txt")
+        myFile.delete()
         for (kocsi in allCarList)
         {
-            myFile.appendText(kocsi.toString())
+            myFile.appendText(kocsi.toSaveFormat())
         }
+    }
+
+    fun LoadIn()
+    {
+        val carStringList:List<String> = File("saves.txt").readLines()
+        var carTempList = mutableListOf<Car>()
+        var splittedCar:List<String>
+        for (kocsiSor in carStringList)
+        {
+            splittedCar = kocsiSor.split(",")
+            carTempList.add(Car(splittedCar[0],CarType.valueOf(splittedCar[1]),splittedCar[2].toInt(),splittedCar[3].toInt()))
+        }
+        allCarList=carTempList
+    }
+
+    override fun toString(): String {
+        var cars:String = ""
+        for(kocsi in allCarList)
+        {
+            cars+= kocsi.toString() + "\n"
+        }
+        return cars
     }
 
 }
--- src/test.kt
+++ src/test.kt
@@ -4,5 +4,7 @@ fun main()
     var kocsiSzalon = DealerShip("szalon","Budapest",123456)
     kocsiSzalon.addNewCar(Car("123-asd",CarType.FERRARI,500,50000000))
     kocsiSzalon.SaveOut()
+    kocsiSzalon.LoadIn()
+    println( kocsiSzalon.toString())
 }
 
